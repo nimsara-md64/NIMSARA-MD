2bbe04d4cb74825a2337bd96b800d9d15318ad79
nimsara-md64 <nimsaramd20@gmail.com>
2025-04-03 02:12:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "kwRSBYwY#YepO8dsFMaxVt3cmI51fP9KDfeqMVQkqbybwVGxszhc",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:IwKbvCdaZbiAgwuRZVgRUgqchoeiSPfC@shortline.proxy.rlwy.net:31127",
   OWNER_NUM: process.env.OWNER_NUM || "94742249044",
 };
