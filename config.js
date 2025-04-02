41ada4810cd9ec9144413b200b0fd3208c0a4ed1
nimsara-md64 <nimsaramd20@gmail.com>
2025-04-02 07:45:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "kwRSBYwY#YepO8dsFMaxVt3cmI51fP9KDfeqMVQkqbybwVGxszhc",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:IwKbvCdaZbiAgwuRZVgRUgqchoeiSPfC@shortline.proxy.rlwy.net:31127",
+  OWNER_NUM: process.env.OWNER_NUM || "94742249044",
 };
