b28aa7a7dadfe0d736f586873bf0b63b27eaf83b
nimsara-md64 <nimsaramd20@gmail.com>
2025-04-17 18:27:43 +0530

    Update img2sticker.js

--- plugins/img2sticker.js
+++ plugins/img2sticker.js
@@ -6,6 +6,7 @@ cmd(
   {
     pattern: "sticker",
     alias: ["s", "stick"],
+    react: "🫠"
     desc: "Convert an image to a sticker",
     category: "utility",
     filename: __filename,
@@ -53,8 +54,8 @@ cmd(
 
       // Create the sticker from the media
       const sticker = new Sticker(media, {
-        pack: "𝐑_𝐎_𝐁_𝐈_𝐍", // Sticker pack name
-        author: "𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋", // Sticker author name
+        pack: "𝐍𝐈𝐌𝐒𝐀𝐑𝐀-𝐌𝐃.. ⚠️💗", // Sticker pack name
+        author: "𝐍_𝐈_𝐌_𝐒_𝐀_𝐑_𝐀", // Sticker author name
         type: StickerTypes.FULL, // Sticker type (FULL or CROPPED)
         quality: 50, // Quality of the output sticker (0–100)
       });
