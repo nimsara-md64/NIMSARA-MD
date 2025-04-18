d83120a897c8b2f65c02071eb67d91e87a90616d
nimsara-md64 <nimsaramd20@gmail.com>
2025-04-18 15:38:25 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,25 +1,38 @@
-
-{cmd , commands} = require('../command')
+const { cmd, commands } = require('../command');
 const config = require('../config');
 
 cmd({
     pattern: "alive",
     alias: "bot",
-    react:"🙃",
+    react: "🙃",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
 },
-async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{ 
-    await robin.sendPresenceUpdate('recording', from);
-    await robin.sendMessage(from, { audio: { url: "https://github.com/nimsara-md64/Bot-helper/raw/refs/heads/main/audio/Bot%20auto%20voice%20.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+async (robin, mek, m, {
+    from, quoted, body, isCmd, command, args, q,
+    isGroup, sender, senderNumber, botNumber2,
+    botNumber, pushname, isMe, isOwner,
+    groupMetadata, groupName, participants,
+    groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        await robin.sendPresenceUpdate('recording', from);
+        await robin.sendMessage(from, {
+            audio: {
+                url: "https://github.com/nimsara-md64/Bot-helper/raw/refs/heads/main/audio/Bot%20auto%20voice%20.mp3"
+            },
+            mimetype: 'audio/mpeg',
+            ptt: true
+        }, { quoted: mek });
 
-return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-    
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+        return await robin.sendMessage(from, {
+            image: { url: config.ALIVE_IMG },
+            caption: config.ALIVE_MSG
+        }, { quoted: mek });
 
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
